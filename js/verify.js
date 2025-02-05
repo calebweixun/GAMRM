8dbcee3f4de667486fb55652a65f8cf5c291d9b8
calebweixun <calebweixun@gmail.com>
2025-02-05 17:19:59 +0800

    update verify page

--- js/verify.js
+++ js/verify.js
@@ -1,12 +1,15 @@
-const validPassword = "7k4*Un";
+function verifycode() {
+  const validPassword = "7k4*Un";
 
-document
-  .getElementById("verifyBtn")
-  .addEventListener("click", function () {
-    const inputPassword = document.getElementById("password").value;
-    if (inputPassword === validPassword) {
-      window.location.href = "userQRScan.html";
-    } else {
-      alert("密碼錯誤，拒絕存取！");
-    }
-  });
+  // 取得使用者輸入的密碼
+  var inputPassword = document.getElementById("password").value;
+
+  // 比對密碼是否正確
+  if (inputPassword === validPassword) {
+    // 密碼正確，跳轉到掃描器頁面
+    window.location.href = "verifyQRScan"; // 請依實際路徑修改
+  } else {
+    // 密碼錯誤，顯示警告訊息
+    alert("密碼錯誤，拒絕存取！");
+  }
+}
